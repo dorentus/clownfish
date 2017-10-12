15c427198e6ffe4d7441d5570361f3496019ee3c
ZHANG Yi <yi.zhang@husor.com>
2017-10-12 16:22:10 +0800

    try fix some issues

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
   if test (clownfish::platform) = "Darwin"
-    clownfish::prompt::rbenv (set_color black; printf "🐠  ") (printf "🍄  ")
+    clownfish::prompt::rbenv (set_color black; printf "🐠  ") (printf "🍄  ")(set_color normal)
   else
-    clownfish::prompt::rbenv (set_color black)
+    clownfish::prompt::rbenv (set_color black)(set_color normal)
   end
 end
