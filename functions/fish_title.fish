1ca553f55c2cfbb1e3417618b30033934c3a3561
ZHANG Yi <zhangyi35@corp.netease.com>
2023-03-06 10:33:09 +0800

    Update functions/fish_title.fish

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,5 +1,5 @@
 function fish_title -d "Tell Apple Terminal about the working directory at each prompt"
   if test (clownfish::platform) = "Darwin"
-    printf '\a\033]7;file://localhost/%s' (clownfish::urlencode $PWD)
+    printf '\033]7;file://localhost/%s\a' (clownfish::urlencode $PWD)
   end
 end
