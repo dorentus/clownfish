98037cdab7473dec687a3911715175e6014b30b3
ZHANG Yi <zhangyi35@corp.netease.com>
2023-03-07 09:37:20 +0800

    rm functions/fish_title.fish

--- functions/clownfish::urlencode.fish
+++
@@ -1,3 +0,0 @@
-function clownfish::urlencode -a url -d "URL Encode"
-  perl -MURI::Escape -le "print uri_escape('$url')"
-end
--- functions/fish_title.fish
+++
@@ -1,5 +0,0 @@
-function fish_title -d "Tell Apple Terminal about the working directory at each prompt"
-  if test (clownfish::platform) = "Darwin"
-    printf '\033]7;file://localhost/%s\a' (clownfish::urlencode $PWD)
-  end
-end
