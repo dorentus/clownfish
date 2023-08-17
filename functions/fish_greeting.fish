c5fb5ae06635b9c517d181c67b978da891677a6b
ZHANG Yi <zhangyi35@corp.netease.com>
2023-08-17 18:04:48 +0800

    play nice with asciinema

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,4 +1,8 @@
 function fish_greeting
+  if test "$ASCIINEMA_REC" = "1"
+    return
+  end
+
   printf "\n"
   if command -s fortune > /dev/null
     if command -s lolcat > /dev/null
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,6 +1,10 @@
 function fish_right_prompt
   if test (clownfish::platform) = "Darwin"
-    clownfish::prompt::rbenv (set_color black; printf "🐠") (printf "🍄")(set_color normal)
+    if test "$ASCIINEMA_REC" = "1"
+      clownfish::prompt::rbenv (set_color black; printf "⏺️") (printf "🍄")(set_color normal)
+    else
+      clownfish::prompt::rbenv (set_color black; printf "🐠") (printf "🍄")(set_color normal)
+    end
   else
     clownfish::prompt::rbenv (set_color black)(set_color normal)
   end
